fdbb9a18cb62284bbed954eebd2da907ec27777d
miss-mimie <mariam.musah55@gmial.com>
2024-11-05 19:09:15 -0800

    Ruby task 7

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# This script matches a string that starts with 'h' and ends with 'n'
-# and can have single character in between
+# This script exactly matches a string that starts with 'h' ends with 'n' and
+# can have any single character in between
 
-puts ARGV[0].scan(/^h.n$/.join
+puts ARGV[0].scan(/^h.n$/).join
